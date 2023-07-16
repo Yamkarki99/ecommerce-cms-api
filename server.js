19b36c93eaf2fb58a6deb442346366aa52b09503
yamkarki99 <99rajeevkarki@gmail.com>
2023-07-16 15:49:24 +1000

    baackend

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
 import express from "express";
+
 const app = express();
 
-app.listen();
+// middlewares
+import morgan from "morgan";
+import cors from "cors";
+import { mongoConnect } from "./src/config/mongoConfig.js";
+
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "server is live",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server is running at http://localhost:8000`);
+});
